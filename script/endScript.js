7d9ed148e5fc609f4c564195ca4481cc2be6fc4f
Vanessi-K <vanessa.koeck.26@outlook.com>
2022-01-21 09:42:49 +0100

    Implemented star rating

--- script/endScript.js
+++ script/endScript.js
@@ -1,17 +1,27 @@
+let power;
+let health;
+let time;
+
 window.onload = () => {
-    let power = localStorage.getItem("power");
-    let health = localStorage.getItem("health");
-    let time = localStorage.getItem("time");
+    readValues();
+    displayValues();
+    displayRating(calculateRating());
+}
 
-    // localStorage.removeItem("heart");
-    // localStorage.removeItem("power");
-    // localStorage.removeItem("time");
+function readValues() {
+    power = localStorage.getItem("power");
+    health = localStorage.getItem("health");
+    time = localStorage.getItem("time");
 
+    localStorage.removeItem("power");
+    localStorage.removeItem("health");
+    localStorage.removeItem("time");
+}
+
+function displayValues() {
     let powerDisplay = document.querySelector("#jar-text");
     let healthDisplay = document.querySelector("#heart-text");
     let timeDisplay = document.querySelector("#time-text");
-
-
 
     powerDisplay.innerText = (power !== null ?  power + "/18" : "0");
     healthDisplay.innerText = (health !== null ?  health  : "0");
@@ -21,9 +31,25 @@ window.onload = () => {
 
     if(time !== null) {
         //time is in milliseconds
-        minutes = Math.floor(+time/1000/60);
-        seconds = Math.round(+time/1000 - minutes*60);
+        minutes = Math.floor(time/1000/60);
+        seconds = Math.round(time/1000 - minutes*60);
     }
 
-    timeDisplay.innerText = minutes + " min " + seconds + " s"
+    timeDisplay.innerText = minutes + " min " + seconds + " s";
+}
+
+function calculateRating() {
+    return Math.round(power/6) + (+health) - Math.round(+time/1000/60);
+}
+
+function displayRating(rating) {
+    for(let i = 1;  i <= rating; i++) {
+        //set star rating
+        let star = document.querySelector("#star-" + i);
+
+        if(star !== null) {
+            star.classList.remove("st-dark");
+            star.classList.add("st-light");
+        }
+    }
 }
